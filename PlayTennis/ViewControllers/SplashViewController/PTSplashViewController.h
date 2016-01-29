8cca8dad5c214bea543d47a51537390bdf6e0ee6
KrishanSunil <KrishanS@Mac02s-Mac-Pro.local>
2016-01-29 15:00:04 +0800

    PlayTennis Initial Project Setup
    Committed on 29/01/2016

---
+++ PlayTennis/ViewControllers/SplashViewController/PTSplashViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTSplashViewController.h
+//  PlayTennis
+//
+//  Created by Krishantha Sunil on 29/1/16.
+//  Copyright © 2016 Krishan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTSplashViewController : UIViewController
+
+@end
---
+++ PlayTennis/ViewControllers/SplashViewController/PTSplashViewController.m
@@ -0,0 +1,37 @@
+//
+//  PTSplashViewController.m
+//  PlayTennis
+//
+//  Created by Krishantha Sunil on 29/1/16.
+//  Copyright © 2016 Krishan. All rights reserved.
+//
+
+#import "PTSplashViewController.h"
+
+@interface PTSplashViewController ()
+
+@end
+
+@implementation PTSplashViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
